338d657316b46f8fe2716cd66efab80b03b308b2
Blinov Vitaly <Blinov.V.A@mail.ru>
2021-02-11 16:22:06 +0400

    Вынес перечисление DAYSLIMIT из класса PlansViewModel

--- Diary_MVC_2.0/Models/PlansViewModel.cs
+++ Diary_MVC_2.0/Models/PlansViewModel.cs
@@ -4,16 +4,16 @@ using System.Collections.Generic;
 
 namespace Diary_MVC_2._0.Models
 {
+    public enum DAYSLIMIT
+    {
+        list,
+        day,
+        week,
+        month,
+    }
+
     public class PlansViewModel
     {
-        public enum DAYSLIMIT
-        {
-            list,
-            day,
-            week,
-            month,
-        }
-
         public List<Plan> Plans { get; set; }
         public SelectList Types { get; set; }
 
